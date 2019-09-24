b8ca0cdf31005f593c6e2d1ae6ef44623acdd010
Jessica McKenzie <contact@jessmckenzie.com>
2019-09-24 11:37:00 +0100

    Added constraints for inclusive number range of 1-100

--- Java03/src/Main.java
+++ Java03/src/Main.java
@@ -1,24 +1,29 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         int n;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Only numbers greater than 1 or less than 100");
+        System.out.println("Only numbers greater than 1 or less than 100 (inclusive)");
         n = scanner.nextInt();
 
-        //Check if even
-        if (n % 2 == 0) {
-            if (n >= 2 && n <= 5) {
-                System.out.println("Not weird");
-            } else if (n >= 6 && n <= 20){
+        if (n>=1 && n<= 100) {
+            //Check if even
+            if (n % 2 == 0) {
+                if (n >= 2 && n <= 5) {
+                    System.out.println("Not weird");
+                } else if (n >= 6 && n <= 20){
+                    System.out.println("Weird");
+                } else if (n > 20) {
+                    System.out.println("Not weird");
+                }
+                //Result for odd
+            } else {
                 System.out.println("Weird");
-            } else if (n > 20) {
-                System.out.println("Not weird");
             }
-            //Result for odd
         } else {
-            System.out.println("Weird");
+            System.out.println("Enter a valid number! (1 - 100)");
         }
     }
 }
